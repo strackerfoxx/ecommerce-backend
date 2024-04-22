700ff75b943add3b71fda6a413443bbe743d509f
Stryker Foxx <hokaygames358@gmail.com>
2024-04-22 02:43:11 -0600

    updateReview

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -36,6 +36,7 @@ export const createReview = async (req, res) => {
                 review.images.push(name)
             });
             product.reviews.push(review._id)
+            review.owner = req.user.id
             await product.save()
             await review.save()
             res.status(200).json({msg: "Review Uploaded Succesfully"})
@@ -45,3 +46,63 @@ export const createReview = async (req, res) => {
     });
 }
 
+export const updateReview = async (req, res) => {
+    const _id = req.query.id;
+    const review = await Review.findById(_id)
+
+    // si el id del usuario no coincide con el id del creador de la review se detiene
+    if(req.user.id !== review.owner.toString()) return res.status(401).json({msg: "unauthorized"})
+    const names = []; 
+
+    const multerConfigurations = {
+        limits: {fileSize: 1000000 * 5},
+        storage: multer.diskStorage({
+            destination: (req, file, cb)  => {
+                cb(null, "./uploads")
+            },
+            filename: (req, file, cb) => {
+                const name = `${string(20)}.jpeg`
+                names.push(`${process.env.BACKEND_URL}/api/products/getimages?img=${name}`)
+                cb(null, name);
+            }
+        })
+    }
+    
+    const upload = multer(multerConfigurations).array("images", 5)
+
+    upload(req, res, async (err) => {
+        if(err)return res.json({msg: err});
+
+        const {title, description, rating} = JSON.parse(req.body.data);
+
+        try {
+            // se revisa si se mandaron imagenes, si es asi se reemplazan a las que habian antes, si no se quedan las que estaban
+            if(names.length > 0){
+                
+                review.images.forEach(image => {
+                    try {
+                        fs.unlinkSync(`./uploads/${image.split("=")[1]}`);
+                    } catch (error) {
+                        return res.status(400).json({msg: error.message})
+                    }
+                });
+                
+                review.images = []
+
+                names.forEach(name => {
+                    review.images.push(name)
+                });
+            }
+
+            // se re asignan los valores a la review con el id mandado campo por campo para poder mandar bien las imagenes
+            review.title = title
+            review.description = description
+            review.rating = rating
+
+            await review.save()
+            res.status(200).json({msg: "Review Updated succesfully"})
+        } catch (error) {
+            return res.status(400).json({msg: error.message})
+        }
+    });
+}
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
 import auth from "../middleware/auth.js"
-import { createReview } from "../controllers/reviewController.js";
+import { createReview, updateReview } from "../controllers/reviewController.js";
 
 router.post("/", auth, createReview)
+router.put("/update", auth, updateReview)
 
 export default router
